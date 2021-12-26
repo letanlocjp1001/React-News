aa7059276f5db0355f3100b8bdd95084dfea4e0a
LE TAN LOC <letanlocjp1001@gmail.com>
2021-12-26 19:17:13 +0900

    update remove Story

--- src/context.js
+++ src/context.js
@@ -11,12 +11,48 @@ import reducer from './reducer'
 
 const API_ENDPOINT = 'https://hn.algolia.com/api/v1/search?'
 
-const initialState = {}
+const initialState = {
+  isLoading: true,
+  hits: [],
+  query: 'react',
+  page: 0,
+  nbPages: 0,
+}
+// Gia tri mac dinh trong reduce la 1 object chứa các thành phần khác nhau
 
 const AppContext = React.createContext()
 
 const AppProvider = ({ children }) => {
-  return <AppContext.Provider value='hello'>{children}</AppContext.Provider>
+  const [state, dispatch] = useReducer(reducer, initialState)
+
+  const fetchStories = async (url) => {
+    dispatch({ type: SET_LOADING })
+    try {
+      const response = await fetch(url)
+      const data = await response.json()
+      dispatch({
+        type: SET_STORIES,
+        payload: { hits: data.hits, nbPages: data.nbPages },
+      })
+      // console.log(data)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  const removeStory = (id) => {
+    dispatch({ type: REMOVE_STORY, payload: id })
+  }
+
+  useEffect(() => {
+    fetchStories(`${API_ENDPOINT}query=${state.query}&page=${state.page}`)
+  }, [])
+
+  return (
+    <AppContext.Provider value={{ ...state, removeStory }}>
+      {children}
+    </AppContext.Provider>
+  )
 }
 // make sure use
 export const useGlobalContext = () => {
